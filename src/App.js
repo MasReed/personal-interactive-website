f782db1cd04b6d157a870dd518f860d9daa92542
MasReed <bluekitdrummer@gmail.com>
2021-08-17 15:54:15 -0500

    add basic html links as a test

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ function App() {
   return (
     <div>
       <h2>Hello, Masons fans. From gh pages.</h2>
+      <a href='http://monsed.com'>homepage</a>
+      <a href='http://restaurant.monsed.com'>Nodo Manager Project</a>
     </div>
   )
 }
