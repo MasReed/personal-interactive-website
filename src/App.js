b2000479f101159f92ba82ce2bf86ffc34649640
MasReed <bluekitdrummer@gmail.com>
2021-08-18 11:07:17 -0500

    implement material-ui to use for styling; ad simple info to site to test themes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,61 @@
+import Box from '@material-ui/core/Box'
+import Button from '@material-ui/core/Button'
+import Container from '@material-ui/core/Container'
 
 function App() {
   return (
-    <div>
-      <h2>Hello, Masons fans. From gh pages.</h2>
-      <a href='http://monsed.com'>homepage</a>
-      <a href='http://restaurant.monsed.com'>Nodo Manager Project</a>
-    </div>
+    <>
+      <Container>
+        <h2>Hello, Masons fans. From gh pages.</h2>
+        <a href='http://monsed.com'>homepage</a>
+        <br />
+        <a href='http://restaurant.monsed.com'>Nodo Manager Project</a>
+      </Container>
+
+      {/* Profile & About Section */}
+      <Box m={0} py={5}>
+        <Container>
+          <h3>Welcome to my curriculum vitae!</h3>
+          <hr />
+          <h3>About Me</h3>
+          <p>
+              I love finding solutions! I'm a self-taught, full stack
+              developer and masterful problem solver. My background is
+              in astrophysics and I have experience in STEM education.
+              I have a passion to learn new things and share them with
+              others. I also enjoy multi-player, strategy
+              games, such as Magic: the Gathering and Catan!
+          </p>
+
+          <img src="images/headshot.png" alt="A bad selfie" />
+          <h3>Details</h3>
+          <p>
+              <strong>Name:</strong>
+              <br />
+              Mason Reed
+              <br />
+
+              <strong>Location:</strong>
+              <br />
+              Midwest, USA
+              <br />
+
+              <strong>Age:</strong>
+              <br />
+              28
+              <br />
+              <br />
+
+              <a href="https://www.instagram.com/mr.time_travel/">mr.time_travel</a>
+          </p>
+
+          <hr />
+          <Button variant='contained'>
+            This is a button
+          </Button>
+        </Container>
+      </Box>
+    </>
   )
 }
 
