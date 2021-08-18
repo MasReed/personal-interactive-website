4062f2a4ddb0e0cf96d889bbdf36e2f67c70ba0b
MasReed <bluekitdrummer@gmail.com>
2021-08-18 14:52:39 -0500

    Use CssBaseline from material-ui

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CssBaseline from '@material-ui/core/CssBaseline'
 // import { ThemeProvider } from '@material-ui/core/styles'
 // import theme from './theme'
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <>
+      <CssBaseline>
+        <App />
+      </CssBaseline>
+    </>
   </React.StrictMode>,
   document.getElementById('root')
 );
