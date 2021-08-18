571d0bf3f8c801b9fbd8e224a0749e5a34895a53
MasReed <bluekitdrummer@gmail.com>
2021-08-18 16:43:20 -0500

    make PageFooter into component, imported to App.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import AppBar from '@material-ui/core/AppBar'
 import Box from '@material-ui/core/Box'
-import Container from '@material-ui/core/Container'
-import Grid from '@material-ui/core/Grid'
-import Link from '@material-ui/core/Link'
 import Tab from '@material-ui/core/Tab'
 import Tabs from '@material-ui/core/Tabs'
 import Toolbar from '@material-ui/core/Toolbar'
 
 import AboutSection from './AboutSection'
+import PageFooter from './PageFooter'
 
 function App() {
   return (
@@ -21,49 +19,19 @@ function App() {
         </Toolbar>
       </AppBar>
 
-      <header>Header</header>
 
-      <div id='#top'></div>
+      <header id='top'>Header</header>
 
-      <div id='#content' style={{ flex: 1 }}>
-        <Box m={0} p={0}>
+      <div id='content' style={{ flex: 1 }}>
+        <Box>
+          <div id='about'></div>
           <AboutSection />
         </Box>
       </div>
 
-      <footer height='200px'>
-        <Box mt={5} p={5} bgcolor='#f5cdaa'>
-          <Container>
-            <Grid container spacing={5}>
-              <Grid item xs={12} sm={4}>
-                <Box py={2}>
-                  <Link href='www.monsed.com' color='inherit'>
-                    Home
-                  </Link>
-                </Box>
-                <Box py={2}>
-                  <Link href='#top' color='inherit'>
-                    To Top
-                  </Link>
-                </Box>
-              </Grid>
-            </Grid>
-          </Container>
-        </Box>
-      </footer>
+      <PageFooter />
     </Box>
   )
 }
 
 export default App;
-
-
-// {/* Navigation */}
-// <AppBar position='sticky'>
-//   <Toolbar>
-//     <Tabs value={0}>
-//       <Tab label='Home' href='http://monsed.com' />
-//       <Tab label='Projects' href='http://restaurant.monsed.com' />
-//     </Tabs>
-//   </Toolbar>
-// </AppBar>
---
+++ src/PageFooter.jsx
@@ -0,0 +1,75 @@
+import React from 'react'
+
+import Box from '@material-ui/core/Box'
+import Container from '@material-ui/core/Container'
+import Grid from '@material-ui/core/Grid'
+import Link from '@material-ui/core/Link'
+
+export default function PageFooter () {
+  return (
+    <footer>
+      <Box m={0} py={5} bgcolor='#f5cdaa'>
+        <Container>
+          <Grid container spacing={5}>
+            {/* Site Map */}
+            <Grid item xs={12} sm={4}>
+              <Box pt={2} borderBottom={1}>
+                <p>Site Map</p>
+              </Box>
+              <Box pt={2}>
+                <Link href='www.monsed.com' color='inherit'>
+                  Home
+                </Link>
+              </Box>
+              <Box pt={2}>
+                <Link href='restaurant.monsed.com' color='inherit'>
+                  Nodo-Manager
+                </Link>
+              </Box>
+            </Grid>
+
+            {/* CV Sections */}
+            <Grid item xs={12} sm={4}>
+              <Box pt={2} borderBottom={1}>
+                <p>Sections</p>
+              </Box>
+              <Box pt={2}>
+                <Link href='#top' color='inherit'>
+                  To Top
+                </Link>
+              </Box>
+              <Box pt={2}>
+                <Link href='#about' color='inherit'>
+                  About
+                </Link>
+              </Box>
+              <Box pt={2}>
+                <Link href='#other' color='inherit'>
+                  Others?
+                </Link>
+              </Box>
+            </Grid>
+
+            {/* About This Site */}
+            <Grid item xs={12} sm={4}>
+              <Box pt={2} borderBottom={1}>
+                <p>About This Site</p>
+              </Box>
+              <Box pt={2}>
+                Whats with the name?
+              </Box>
+              <Box pt={2}>
+                <Link href='http://www.github.com/MasReed/personal-interactive-website' color='inherit'>
+                  Github
+                </Link>
+              </Box>
+            </Grid>
+          </Grid>
+          <Box textAlign='center' mt={3} mb={0} pt={9} pb={2}>
+            MasReed &reg; { new Date().getFullYear() }
+          </Box>
+        </Container>
+      </Box>
+    </footer>
+  )
+}
