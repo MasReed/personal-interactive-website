117f4532cc71a2a0b517914422ed8f7d163d506b
MasReed <bluekitdrummer@gmail.com>
2021-08-20 12:13:59 -0500

    update tech skills to utilize list components

--- src/TechContent.jsx
+++ src/TechContent.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 import Box from '@material-ui/core/Box'
 import Container from '@material-ui/core/Container'
 import Grid from '@material-ui/core/Grid'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
 
 export default function TechContent() {
   return (
@@ -11,43 +13,60 @@ export default function TechContent() {
         <Grid container spacing={5}>
           <Grid item sm={3}>
             <h2>Languages</h2>
-            <Box style={{ color: '#EBF2FF'}}>
-              <p>Javascript/ECMAscript</p>
-              <p>MongoDB Query Language (MQL)</p>
-              <p>Python</p>
-              <p>HTML/CSS</p>
-              <p>SQL</p>
-            </Box>
+            <List style={{ color: '#EBF2FF'}}>
+              <ListItem>
+                Javascript/ECMAscript
+              </ListItem>
+              <ListItem>
+                MongoDB Query Language (MQL)
+              </ListItem>
+              <ListItem>
+                Python
+              </ListItem>
+              <ListItem>
+                HTML/CSS
+              </ListItem>
+              <ListItem>
+                SQL
+              </ListItem>
+            </List>
           </Grid>
           <Grid item sm={3}>
             <h2>Technologies</h2>
-            <Box style={{ color: '#EBF2FF'}}>
-              <p>Git & Github</p>
-              <p>Bootstrap Framework</p>
-              <p>Material-ui Framework</p>
-              <p>jQuery</p>
-              <p>Heroku</p>
-            </Box>
+            <List style={{ color: '#EBF2FF'}}>
+              <ListItem>Git & Github</ListItem>
+              <ListItem>Bootstrap Framework</ListItem>
+              <ListItem>Material-ui Framework</ListItem>
+              <ListItem>jQuery</ListItem>
+              <ListItem>Heroku</ListItem>
+            </List>
           </Grid>
           <Grid item sm={3}>
             <h2>Utilities</h2>
-            <Box style={{ color: '#EBF2FF'}}>
-              <p>MS Office 365 Suite</p>
-              <p>Teams/Zoom</p>
-              <p>LaTeX</p>
-              <p>Markdown</p>
-              <p>Slack/Discord</p>
-            </Box>
+            <List style={{ color: '#EBF2FF'}}>
+              <ListItem>MS Office 365 Suite</ListItem>
+              <ListItem>Teams/Zoom</ListItem>
+              <ListItem>LaTeX</ListItem>
+              <ListItem>Markdown</ListItem>
+              <ListItem>Slack/Discord</ListItem>
+            </List>
           </Grid>
           <Grid item sm={3}>
             <h2>Other</h2>
             <Box style={{ color: '#EBF2FF'}}>
-              <p>Arduino</p>
-              <p>Unreal Engine 4</p>
-              <p>C++, RobotC</p>
-              <p>FreeCAD</p>
-              <p>VEX Robotics</p>
+              <p></p>
+              <p></p>
+              <p></p>
+              <p></p>
+              <p></p>
             </Box>
+            <List style={{ color: '#EBF2FF'}}>
+              <ListItem>Arduino</ListItem>
+              <ListItem>Unreal Engine 4</ListItem>
+              <ListItem>C++, RobotC</ListItem>
+              <ListItem>FreeCAD</ListItem>
+              <ListItem>VEX Robotics</ListItem>
+            </List>
           </Grid>
         </Grid>
       </Container>
