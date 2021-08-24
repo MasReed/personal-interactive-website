65cae5634da597eb89e9b9cc38c9ad7ac0844cd5
MasReed <bluekitdrummer@gmail.com>
2021-08-23 21:04:16 -0500

    update TechContent to utilize makeStyles hook instead of HoC

--- src/TechContent.jsx
+++ src/TechContent.jsx
@@ -5,8 +5,9 @@ import Container from '@material-ui/core/Container'
 import Grid from '@material-ui/core/Grid'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
+import Typography from '@material-ui/core/Typography'
 
-import { withStyles } from '@material-ui/core/styles'
+import { makeStyles } from '@material-ui/core/styles'
 
 export default function TechContent() {
 
@@ -41,29 +42,39 @@ export default function TechContent() {
     ],
   }
 
-  const StyledListItem = withStyles({
-    root: {
+  const useStyles = makeStyles({
+    heading: {
+      color: '#f5cdaa'
+    },
+    listItemStyle: {
       backgroundColor: '#49464d',
       borderRadius: 3,
       border: '1px solid #ea5e42',
       color: '#EBF2FF',
       margin: '1px 0'
-    },
-  })(ListItem)
+    }
+  })
+
+  const classes = useStyles()
 
   return (
-    <Box style={{ color: '#f5cdaa'}}>
-      <Container>
+    <Box>
+      <Container style={{ margin: '0', padding: '0 16px' }}>
         <Grid container spacing={4}>
           {
             Object.keys(techContent).map((key) => {
               return (
                 <Grid item sm={3} key={key}>
-                  <h2>{key[0].toUpperCase() + key.slice(1)}</h2>
+                  <h2 className={classes.heading}>{key[0].toUpperCase() + key.slice(1)}</h2>
                   <List>
                     {
                       techContent[key].map(item => (
-                        <StyledListItem key={item.toString()}>{item}</StyledListItem>
+                        <ListItem
+                          key={item.toString()}
+                          className={classes.listItemStyle}
+                        >
+                          {item}
+                        </ListItem>
                       ))
                     }
                   </List>
