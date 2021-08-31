5f4b509c5a65687489c6ad706a33abf6bdcde89b
MasReed <bluekitdrummer@gmail.com>
2021-08-31 14:04:56 -0500

    PageFooter now generates links from json data

--- src/PageFooter.jsx
+++ src/PageFooter.jsx
@@ -1,115 +1,125 @@
 import React from 'react'
 
+import { makeStyles, useTheme } from '@material-ui/core/styles'
 import Box from '@material-ui/core/Box'
 import Container from '@material-ui/core/Container'
+import Divider from '@material-ui/core/Divider'
 import Grid from '@material-ui/core/Grid'
 import Link from '@material-ui/core/Link'
+import Typography from '@material-ui/core/Typography'
 
-import theme from './theme'
+import siteMap from './content/siteMap.json'
+import sectionInfo from './content/sectionInfo.json'
+
+const useStyle = makeStyles((theme) => ({
+  contentSpacingStyle: {
+    margin: '3rem 0',
+    padding: '0 2rem'
+  },
+  footerBox: {
+    backgroundColor: theme.palette.orange.main,
+    margin: '2rem 0',
+    padding: '2rem'
+  },
+  footerStyle: {
+    backgroundColor: theme.palette.orange.main,
+    margin: 0,
+    padding: '2rem 0'
+  },
+}))
 
 export default function PageFooter () {
+  const theme = useTheme()
+  const classes = useStyle(theme)
+
   return (
-    <footer>
-      <Box m={0} py={5} style={{ backgroundColor: theme.palette.orange.main }}>
-        <Container>
+    <Box id='footer' component='footer' className={classes.footerStyle}>
+      <Container>
+        {/* Main Content */}
+        <Box className={classes.contentSpacingStyle}>
           <Grid container spacing={5}>
-            {/* Site Map */}
+
+            {/* Site Map Links */}
             <Grid item xs={12} sm={4}>
-              <Box pt={2} borderBottom={1}>
-                <p>Site Map</p>
+              <Box py={2}>
+                <Typography variant='h6'>Site Map</Typography>
               </Box>
-              <Box pt={2}>
-                <Link
-                  href='www.monsed.com'
-                  color='inherit'
-                  target='_blank'
-                  rel='noopener'
-                >
-                  Home
-                </Link>
-              </Box>
-              <Box pt={2}>
-                <Link
-                  href='restaurant.monsed.com'
-                  color='inherit'
-                  target='_blank'
-                  rel='noopener'
-                >
-                  Nodo-Manager
-                </Link>
-              </Box>
+              <Divider />
+              {
+                siteMap.map(link => (
+                  <Box key={link.url} pt={2}>
+                    <Typography
+                      component={Link}
+                      href={link.url}
+                      target='_blank'
+                      rel='noopener'
+                      color='textPrimary'
+                      variant='body2'
+                    >
+                      {link.text}
+                    </Typography>
+                  </Box>
+                ))
+              }
             </Grid>
 
-            {/* CV Sections */}
+            {/* CV Section Jump Links*/}
             <Grid item xs={12} sm={4}>
-              <Box pt={2} borderBottom={1}>
-                <p>Sections</p>
+              <Box py={2}>
+                <Typography variant='h6'>Sections</Typography>
               </Box>
-              <Box pt={2}>
-                <Link href='#top' color='inherit'>
-                  To Top
-                </Link>
-              </Box>
-              <Box pt={2}>
-                <Link href='#about' color='inherit'>
-                  About
-                </Link>
-              </Box>
-              <Box pt={2}>
-                <Link href='#tech-skills' color='inherit'>
-                  Tech Skills
-                </Link>
-              </Box>
-              <Box pt={2}>
-                <Link href='#projects' color='inherit'>
-                  Projects
-                </Link>
-              </Box>
-              <Box pt={2}>
-                <Link href='#education' color='inherit'>
-                  Education
-                </Link>
-              </Box>
-              <Box pt={2}>
-                <Link href='#experience' color='inherit'>
-                  Experience
-                </Link>
-              </Box>
-              <Box pt={2}>
-                <Link href='#contact' color='inherit'>
-                  Contact
-                </Link>
-              </Box>
+              <Divider />
+
+              {
+                sectionInfo.map((section) => (
+                  <Box key={section.id} pt={2}>
+                    <Typography
+                      component={Link}
+                      href={section.hashLink}
+                      color='textPrimary'
+                      variant='body2'
+                    >
+                      {section.linkText}
+                    </Typography>
+                  </Box>
+                ))
+              }
             </Grid>
 
             {/* About This Site */}
             <Grid item xs={12} sm={4}>
-              <Box pt={2} borderBottom={1}>
-                <p>About This Site</p>
+              <Box py={2}>
+                <Typography variant='h6'>About This Site</Typography>
               </Box>
+              <Divider />
               <Box pt={2}>
                 Whats with the name?
               </Box>
               <Box pt={2}>
-                <Link
+                <Typography
+                  component={Link}
                   href='http://www.github.com/MasReed/personal-interactive-website'
-                  color='inherit'
                   target='_blank'
                   rel='noopener'
+                  color='textPrimary'
+                  variant='body2'
                 >
                   Github
-                </Link>
-              </Box>
-              <Box>
-                <p>Mostly coded during an 8hr roadtrip to Ohio</p>
+                </Typography>
               </Box>
             </Grid>
           </Grid>
-          <Box textAlign='center' mt={3} mb={0} pt={9} pb={2}>
+        </Box>
+
+        <Divider />
+
+        {/* Copyright Tag */}
+        <Box text mt={2} mb={0} py={4}>
+          <Typography align='center' variant='body1'>
             MasReed &reg; { new Date().getFullYear() }
-          </Box>
-        </Container>
-      </Box>
-    </footer>
+          </Typography>
+        </Box>
+      </Container>
+    </Box>
   )
 }
